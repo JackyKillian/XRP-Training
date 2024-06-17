d530287e1d6145a3c63989743f719b4c9ebfbeda
JackyKillian <Jackkillian29@gmail.com>
2024-06-17 16:42:16 -0400

    created a simple drive comand

---
+++ src/main/java/frc/robot/commands/simlpeatoucommand.java
@@ -0,0 +1,8 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class simlpeatoucommand extends SequentialCommandGroup {
+
+    addCommands(new drivefortimecode(drivetrain 2.0,1.0,0.0);
+}
